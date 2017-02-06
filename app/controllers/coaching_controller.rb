2997ec3a92efe8babde7823c40f37b117c3795f7
laura crief <crieflaura@gmail.com>
2017-02-06 14:24:37 +0100

    exercice done

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,17 @@
+class CoachingController < ApplicationController
+  def ask
+    @query = params[:query]
+  end
+
+  def answer
+    @query = params[:query]
+    if @query.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @query == "I am going to work right now!"
+      @answer = ""
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
